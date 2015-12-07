cda25226eb499ecdb1b3b274e6ccf3482d9d7974
Neel Patel <neelpatel@Neels-MacBook-Pro.local>
2015-12-06 23:10:17 -0500

    Adding region lookup

---
+++ assign_region.py
@@ -0,0 +1,40 @@
+import csv
+import us
+import zipcode
+
+
+def get_abbr(state):
+	lookup_value = us.states.lookup(unicode(state))
+	if lookup_value:
+		return lookup_value.abbr
+	else:
+		return None
+
+def get_region_mapping():
+	mapping = {}
+	with open('regions.txt', "r+") as inputfile:
+		reader = csv.reader(inputfile)
+
+		for row in reader:
+			region = row[0]
+			states = row[1:]			
+
+			state_abbreviations = [get_abbr(x) for x in states]
+
+			for abbreviation, state in zip(state_abbreviations, states):
+				mapping[state] = region
+				mapping[abbreviation] = region
+
+	mapping[None] = "unknown"
+
+	return mapping
+
+def get_region_from_zip(zip_code, mapping=None):
+	if not mapping:
+		mapping = get_region_mapping()
+
+	zip_code_object = zipcode.isequal(zip_code)
+
+	return mapping[zip_code_object.state]
+
+
