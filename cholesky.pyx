68583e08c34160e80bbc6f8748ee7a51d7e8efd7
Nithin Tumma <nithin@Nithins-MacBook-Pro.local>
2015-12-08 22:25:53 -0500

    kinda working

--- cholesky.pyx
+++ cholesky.pyx
@@ -1,18 +1,21 @@
 from cython.parallel import prange
+from libc.math cimport sqrt
+
 import numpy as np
 cimport numpy as np
 
-cpdef np.float_64_t[:, :] cholesky(np.float_64_t[:, :] mat, int num_threads):
+cpdef np.float64_t[:, :] cholesky(np.float64_t[:, :] mat, int num_threads):
     cdef: 
         # put definitions in here 
         int i, j, k
         int rows, cols
-    rows, cols = mat.shape
+    rows = mat.shape[0]
+    cols = mat.shape[1]
     with nogil:
         for i in range(rows):
-            mat[i,i] = np.sqrt(mat[i, i])
+            mat[i,i] = sqrt(mat[i, i])
             # each iteration updates/writes to same part of matrix 
-            for j in prange(k+1, rows, num_threads=num_threads):
+            for j in prange(i+1, rows, num_threads=num_threads):
                 mat[i, j] = mat[i, j] / mat[i, i]
             # will never update before reading 
             for k in prange(i+1, rows, num_threads=num_threads):
--- test.py
+++ test.py
@@ -3,9 +3,8 @@ import numpy as np
 import time
 
 import pyximport
-pyximport.install()
-
-from cholesky import cholesky 
+pyximport.install(setup_args={"include_dirs":np.get_include()})
+import cholesky 
 
 
 def test_time():
@@ -13,12 +12,15 @@ def test_time():
     results = np.linalg.lstsq(X[:-1, :], X[:-1, -1])
     print time.time() - start
 
-def test_cholesky():
-    X = np.random.rand(100000000, 25)
-    x_t_x = np.cov(X)
+def test_cholesky(size, n_threads):
+    X = np.random.rand(size + 1, size)
+    x_t_x = np.cov(X.T)
     x_t_x_2 = np.copy(x_t_x)
-    ch_x = cholesky(x_t_x, 1)
+    print "About to start cholesky"
+    start = time.time()
+    ch_x = np.asarray(cholesky.cholesky(x_t_x, n_threads)).T
+    print "Cholesky time for size: ", size, time.time() - start
     real_ch_x = np.linalg.cholesky(x_t_x_2)
-    assert(np.array_equal(ch_x, real_ch_x))
+    assert(np.allclose(ch_x, real_ch_x))
 
-test_cholesky()
+test_cholesky(10000, 2)
